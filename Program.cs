e586f64fbb2c99024dc0b5dbf4a9df3837716352
MitayiSana <mitayisana@gmail.com>
2024-10-17 13:43:51 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection.Metadata;
+
+namespace FormatExceptions
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            string line = Console.ReadLine();
+            try
+            {
+                a = int.Parse(line);
+               
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Zero is not integer not string",line);
+            }
+            
+        }
+    }
+}
